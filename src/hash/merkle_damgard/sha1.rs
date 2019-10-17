42e9d1b0f5d990cece6d4ac0735668c2034c6955
Cydhra <ubezl@student.kit.edu>
2019-10-17 21:40:04 +0200

    broken: implemented sha1. Not working yet

--- src/hash/merkle_damgard/sha1.rs
+++ src/hash/merkle_damgard/sha1.rs
@@ -1,3 +1,9 @@
+use std::mem;
+use std::mem::size_of;
+
+use crate::array_util;
+use crate::hash::merkle_damgard::MerkleDamagardHash;
+
 const BLOCK_LENGTH_BYTES: usize = 64;
 const BLOCK_LENGTH_DOUBLE_WORDS: usize = BLOCK_LENGTH_BYTES / 4;
 
@@ -11,27 +17,50 @@ pub struct SHA1Hash {
     pub e: u32,
 }
 
-impl SHA1Hash {
-    /// Create and return the initial state of an MD5 hash before any data has been digested.
-    pub const fn initial() -> SHA1Hash {
-        return SHA1Hash { message_length: 0, a: 0x67452301, b: 0xEFCDAB89, c: 0x98BADCFE, d: 0x10325476, e: 0xC3D2E1F0 };
-    }
+impl MerkleDamagardHash for SHA1Hash {
+    const INITIAL: Self = SHA1Hash { message_length: 0, a: 0x67452301, b: 0xEFCDAB89, c: 0x98BADCFE, d: 0x10325476, e: 0xC3D2E1F0 };
 
-    pub fn round_function(&mut self, input_block: &[u32]) {
-        assert_eq!(input_block.len(), BLOCK_LENGTH_DOUBLE_WORDS);
+    const BLOCK_SIZE: usize = BLOCK_LENGTH_BYTES;
+
+    fn round_function(&mut self, input_block: &[u8]) {
+        assert_eq!(input_block.len(), Self::BLOCK_SIZE);
+
+        let mut extended_block = [0u32; 80];
+        unsafe { array_util::align_to_u32a_le(&mut extended_block[0..16], input_block) };
+
+        for i in 16..80 {
+            extended_block[i] = u32::rotate_left(
+                extended_block[i - 3] ^ extended_block[i - 8] ^ extended_block[i - 14] ^ extended_block[i - 16], 1)
+        }
 
         let mut round_state = *self;
 
         for i in 0..80 {
-//            let (scrambled_data, magic_constant) = match i {
-//
-//                _ => unreachable!()
-//            };
+            let (scrambled_data, magic_constant) = match i {
+                0..=19 => {
+                    ((round_state.b & round_state.c) | ((!round_state.b) & round_state.d), 0x5A827999)
+                },
+                20..=39 => {
+                    (round_state.b ^ round_state.c ^ round_state.d, 0x6ED9EBA1)
+                },
+                40..=59 => {
+                    ((round_state.b & round_state.c) | (round_state.b & round_state.d)
+                         | (round_state.c & round_state.d), 0x8F1BBCDC)
+                },
+                60..=79 => {
+                    (round_state.b ^ round_state.c ^ round_state.d, 0xCA62C1D6)
+                },
+                _ => unreachable!()
+            };
 
-            let temp = panic!();
+            let temp = u32::rotate_left(round_state.a, 5)
+                .wrapping_add(scrambled_data)
+                .wrapping_add(round_state.e)
+                .wrapping_add(magic_constant)
+                .wrapping_add(extended_block[i]);
             round_state.e = round_state.d;
             round_state.d = round_state.c;
-            round_state.c = panic!();
+            round_state.c = u32::rotate_left(round_state.b, 30);
             round_state.b = round_state.a;
             round_state.a = temp;
         }
@@ -41,5 +70,49 @@ impl SHA1Hash {
         self.c = self.c.wrapping_add(round_state.c);
         self.d = self.d.wrapping_add(round_state.d);
         self.e = self.e.wrapping_add(round_state.e);
+    }
+
+    //noinspection DuplicatedCode
+    fn digest_last_block(&mut self, input: &[u8]) {
+        let message_length_bits: u64 = (input.len() as u64) * 8u64;
+        let message_blocks_count = input.len() / BLOCK_LENGTH_BYTES;
+
+        let relevant_data = &input[message_blocks_count * BLOCK_LENGTH_BYTES..];
+
+        let mut last_block = [0u8; BLOCK_LENGTH_BYTES];
+        // append the last part of message to the block
+        for (dst, src) in last_block.iter_mut().zip(relevant_data.iter()) {
+            *dst = *src
+        }
+
+        // append a single 1-bit to the end of the message
+        last_block[relevant_data.len()] = 0x80u8;
+
+        // if there is not enough space for the message length to be appended, a new block must be
+        // created
+        if relevant_data.len() + 1 + size_of::<u64>() > BLOCK_LENGTH_BYTES {
+            let mut overflow_block = [0u8; BLOCK_LENGTH_BYTES];
+            // append the message length in bits
+            for i in 0..8 {
+                overflow_block[BLOCK_LENGTH_BYTES - 8 + i] = (message_length_bits >> (i * 8) as u64) as u8;
+            }
+
+            self.round_function(&last_block);
+            self.round_function(&overflow_block);
+        } else {
+            // append the message length in bits
+            for i in 0..8 {
+                last_block[56 + i] = (message_length_bits >> (i * 8) as u64) as u8;
+            }
+
+            self.round_function(&last_block);
+        }
+    }
+}
+
+impl SHA1Hash {
+    /// Generates a raw ``[u8; 16]`` array from the current hash state.
+    pub fn to_raw(&self) -> [u8; 20] {
+        unsafe { mem::transmute::<[u32; 5], [u8; 20]>([self.a, self.b, self.c, self.d, self.e]) }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,24 @@ mod tests {
 
     use crate::hash::merkle_damgard::md5::MD5Hash;
     use crate::hash::merkle_damgard::MerkleDamagardHash;
+    use crate::hash::merkle_damgard::sha1::SHA1Hash;
 
     #[test]
     fn test_md5() {
-        assert_eq!("d41d8cd98f00b204e9800998ecf8427e",
-                   hex::encode(MD5Hash::digest_message(&"".as_bytes()).to_raw()));
-        assert_eq!("5748be477f8cab2e6d785cd2412b823c",
-                   hex::encode(MD5Hash::digest_message(
-                       &"a-very-long-message-that-cannot-be-digested-at-once".as_bytes()).to_raw()))
+        assert_eq!(hex::encode(MD5Hash::digest_message(&"".as_bytes()).to_raw()),
+                   "d41d8cd98f00b204e9800998ecf8427e");
+        assert_eq!(hex::encode(MD5Hash::digest_message(
+                       &"a-very-long-message-that-cannot-be-digested-at-once".as_bytes()).to_raw()),
+                   "5748be477f8cab2e6d785cd2412b823c")
+    }
+
+    #[test]
+    fn test_sha1() {
+        assert_eq!(hex::encode(SHA1Hash::digest_message(&"".as_bytes()).to_raw()),
+                   "da39a3ee5e6b4b0d3255bfef95601890afd80709",);
+        assert_eq!(hex::encode(SHA1Hash::digest_message(
+                       &"a-very-long-message-that-cannot-be-digested-at-once".as_bytes()).to_raw()),
+                   "fc0557cb580c6cc1949f126d0020ef6e7eadba7d")
     }
 }
 
