92b82e6464bfab98a916499badead061d98b9970
Cydhra <ubezl@student.kit.edu>
2019-10-20 15:31:45 +0200

    basic unimplemented trait impls for algebra_derive

--- jester_algebra/jester_algebra_derive/src/lib.rs
+++ jester_algebra/jester_algebra_derive/src/lib.rs
@@ -15,12 +15,97 @@ pub fn prime_field_derive(input: TokenStream) -> TokenStream {
 /// modified AST with the new members.
 fn generate_prime_field(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
+
     let gen = quote! {
-        use num::Num;
+        impl Add<#name> for #name {
+            type Output = #name;
+
+            fn add(self, rhs: #name) -> Self::Output {
+                unimplemented!()
+            }
+        }
+
+        impl Sub<#name> for #name {
+            type Output = #name;
+
+            fn sub(self, rhs: #name) -> Self::Output {
+                unimplemented!()
+            }
+        }
+
+        impl Div<#name> for #name {
+            type Output = #name;
+
+            fn div(self, rhs: #name) -> Self::Output {
+                unimplemented!()
+            }
+        }
+
+        impl Mul<#name> for #name {
+            type Output = #name;
+
+            fn mul(self, rhs: #name) -> Self::Output {
+                unimplemented!()
+            }
+        }
+
+        impl Rem<#name> for #name {
+            type Output = Self;
+
+            fn rem(self, rhs: #name) -> #name {
+                unimplemented!()
+            }
+        }
+
+        impl Zero for #name {
+            fn zero() -> Self {
+                #name(BigInt::zero())
+            }
+
+            fn is_zero(&self) -> bool {
+                self.0.is_zero()
+            }
+        }
+
+        impl One for #name {
+            fn one() -> Self {
+                #name(BigInt::one())
+            }
+
+            fn is_one(&self) -> bool
+                where Self: PartialEq, {
+                self.0.is_one()
+            }
+        }
 
         impl Num for #name {
+            type FromStrRadixErr = ParseBigIntError;
 
+            fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> {
+                unimplemented!()
+            }
         }
+
+        impl Sum for #name {
+            fn sum<I: Iterator<Item=Self>>(iter: I) -> Self {
+                let mut tmp = #name::zero();
+                for x in iter {
+                    tmp = tmp + x;
+                }
+                tmp
+            }
+        }
+
+        impl Product for #name {
+            fn product<I: Iterator<Item=Self>>(iter: I) -> Self {
+                let mut tmp = #name::one();
+                for x in iter {
+                    tmp = tmp * x;
+                }
+                tmp
+            }
+        }
+
     };
     gen.into()
 }
--- jester_algebra/src/lib.rs
+++ jester_algebra/src/lib.rs
@@ -1,3 +1,13 @@
-trait PrimeField {
+use std::iter::{Product, Sum};
+use std::ops::{Add, Div, Mul, Rem, Sub};
 
-}
+use num::{Integer, Num, One, pow::pow, Signed, Zero};
+use num_bigint::ParseBigIntError;
+use num_bigint::BigInt;
+
+use jester_algebra_derive::PrimeField;
+
+#[derive(PrimeField, Debug, Clone, PartialEq, PartialOrd)]
+struct Mersenne89(BigInt);
+
+trait PrimeField {}
