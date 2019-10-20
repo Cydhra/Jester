1d8e8c8a0adddb41b718f09005da43e4a96e90b5
Cydhra <ubezl@student.kit.edu>
2019-10-20 15:21:04 +0200

    added syn, quote, num and implemented simple derive trait for jester_algebra_derive

--- jester_algebra/jester_algebra_derive/src/lib.rs
+++ jester_algebra/jester_algebra_derive/src/lib.rs
@@ -1,8 +1,26 @@
 extern crate proc_macro;
 
+use syn;
+
+use quote::quote;
+
 use crate::proc_macro::TokenStream;
 
 #[proc_macro_derive(PrimeField)]
 pub fn prime_field_derive(input: TokenStream) -> TokenStream {
-    unimplemented!()
+    generate_prime_field(&syn::parse(input).expect("cannot parse token stream"))
+}
+
+/// Generate a prime field from the input given as an AST structure and return it as a token stream containing a
+/// modified AST with the new members.
+fn generate_prime_field(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        use num::Num;
+
+        impl Num for #name {
+
+        }
+    };
+    gen.into()
 }
