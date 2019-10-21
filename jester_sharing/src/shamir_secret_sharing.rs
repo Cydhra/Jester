dfefa8c602c0f5aed6d4f1a31d29f456ffda2a86
Cydhra <ubezl@student.kit.edu>
2019-10-22 00:34:10 +0200

    implemented shamir secret sharing algorithm

--- jester_sharing/src/shamir_secret_sharing.rs
+++ jester_sharing/src/shamir_secret_sharing.rs
@@ -1,13 +1,17 @@
-use num::Num;
+use num::{BigUint, FromPrimitive};
+use num::pow::pow;
+use num_bigint::RandBigInt;
 use rand::{CryptoRng, RngCore};
+
+use jester_algebra::PrimeField;
 
 use crate::ThresholdSecretSharingScheme;
 
 /// Shamir's secret sharing scheme that uses polynomials of `threshold` degree and solutions of it as shares.
 pub struct ShamirSecretSharing;
 
-impl<T> ThresholdSecretSharingScheme<T, T> for ShamirSecretSharing
-    where T: Num + Clone {
+impl<T> ThresholdSecretSharingScheme<T, (usize, T)> for ShamirSecretSharing
+    where T: PrimeField + Clone {
     /// Generate a random polynomial `f` and `count` solutions `sn = f(n)` where `n != 0` as shares. The secret is
     /// the solution `secret = f(0)` of the polynomial and each share is the solution of `f(i)` where `i - 1` is the
     /// index within the returned vector.
@@ -19,9 +23,19 @@ impl<T> ThresholdSecretSharingScheme<T, T> for ShamirSecretSharing
     ///
     /// #Output
     /// Returns a vector of `count` shares
-    fn generate_shares<R>(rng: &mut R, secret: &T, count: usize, threshold: usize) -> Vec<T>
-        where R: RngCore + CryptoRng {
-        unimplemented!()
+    fn generate_shares<R>(rng: &mut R, secret: &T, count: usize, threshold: usize) -> Vec<(usize, T)>
+        where R: RngCore + CryptoRng + RandBigInt {
+        assert!(threshold > 1);
+
+        let polynomial = (1..threshold)
+            .map(|i| (i, T::generate_random_member(rng)))
+            .collect::<Vec<_>>();
+
+        (1..=count)
+            .map(|x| (x, polynomial.clone().iter()
+                .fold(secret.clone(), |akk, (index, val)|
+                    akk + val.clone() * BigUint::from_usize(pow(x, *index)).unwrap().into())))
+            .collect()
     }
 
     /// Interpolates the secret using the Lagrange interpolation method.
@@ -33,21 +47,42 @@ impl<T> ThresholdSecretSharingScheme<T, T> for ShamirSecretSharing
     /// #Output:
     /// Given that `threshold` matches the threshold at generation and enough shares are present, it will return an
     /// instance of `T` that is reconstructed from the shares
-    fn reconstruct_secret(shares: &[T], threshold: usize) -> T {
-        unimplemented!()
+    fn reconstruct_secret(shares: &[(usize, T)], threshold: usize) -> T {
+        shares.iter()
+            .take(threshold)
+            .map(|(i, share)|
+                share.clone().mul(shares.iter()
+                    .filter(|(j, _)| *i != *j)
+                    .map(|(j, _)| {
+                        T::from_isize(-(*j as isize)).unwrap().mul(T::from_isize(*i as isize - *j as isize).unwrap()
+                            .inverse())
+                    })
+                    .product()))
+            .sum()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use num::Num;
+    use num::{FromPrimitive, One};
+    use rand::thread_rng;
+
+    use jester_algebra::Mersenne89;
 
     use crate::ThresholdSecretSharingScheme;
 
     use super::ShamirSecretSharing;
 
     #[test]
-    fn test() {
+    fn test_generator() {
+        let shares = ShamirSecretSharing::generate_shares(&mut thread_rng(), &Mersenne89::one(), 5, 5);
+        assert_eq!(shares.len(), 5)
+    }
 
+    #[test]
+    fn test() {
+        let shares = ShamirSecretSharing::generate_shares(&mut thread_rng(),
+                                                          &Mersenne89::from_usize(20).unwrap(), 5, 5);
+        assert_eq!(ShamirSecretSharing::reconstruct_secret(&shares, 5), Mersenne89::from_usize(20).unwrap());
     }
 }
