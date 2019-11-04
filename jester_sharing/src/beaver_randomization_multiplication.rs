1c946fd977953ab875adb16ab6eb8d7800cc4924
Cydhra <ubezl@student.kit.edu>
2019-11-04 21:31:42 +0100

    implemented parallel multiplication using join! of the async-await feature

--- jester_sharing/src/beaver_randomization_multiplication.rs
+++ jester_sharing/src/beaver_randomization_multiplication.rs
@@ -1,6 +1,7 @@
 use std::future::Future;
 use std::pin::Pin;
 
+use futures::{future::join_all, join};
 use num::traits::NumOps;
 
 use jester_algebra::PrimeField;
@@ -18,18 +19,41 @@ pub trait BeaverRandomizationMultiplication<T, S>: ParallelMultiplicationScheme<
 
     /// Obtain random triples of shares `([a], [b], [c])` where `c = a * b` holds. Every participant must use shares
     /// of the same triple during the same multiplication. This function cannot be called in parallel.
-    fn obtain_beaver_triples<'a>(&'a mut self, count: usize) -> Pin<Box<dyn Future<Output=(S, S, S)> + Send + 'a>>;
+    fn obtain_beaver_triples<'a>(&'a mut self, count: usize) -> Pin<Box<dyn Future<Output=Vec<(S, S, S)>> + Send + 'a>>;
 }
 
 impl<P, T, S> ParallelMultiplicationScheme<T, S> for P
-    where T: PrimeField + 'static,
-          S: NumOps<T, S> + Clone + Send + Sync + 'static,
+    where T: PrimeField + Send + 'static,
+          S: NumOps<T, S> + NumOps + Clone + Send + Sync + 'static,
           P: BeaverRandomizationMultiplication<T, S> + ThresholdSecretSharingScheme<T, S> + LinearSharingScheme<S> +
-          CliqueCommunicationScheme<T, S> + MultiplicationScheme<T, S> {
+          CliqueCommunicationScheme<T, S> + MultiplicationScheme<T, S> + Send {
     fn parallel_multiply<'a>(&'a mut self, pairs: &[(S, S)]) -> Pin<Box<dyn Future<Output=Vec<S>> + Send + 'a>> {
-        let _pairs_clone = pairs.iter().map(|(l, r)| (l.clone(), r.clone())).collect::<Vec<(S, S)>>();
+        let pairs_clone = pairs.iter().map(|(l, r)| (l.clone(), r.clone())).collect::<Vec<(S, S)>>();
 
-        unimplemented!()
+        Box::pin(
+            async move {
+                let beaver_triples = self.obtain_beaver_triples(pairs_clone.len()).await;
+
+                let multiplications = pairs_clone
+                    .into_iter()
+                    .zip(beaver_triples)
+                    .map(|((lhs, rhs), (a, b, c))| {
+                        let epsilon_share = Self::sub_shares(&lhs, &a);
+                        let delta_share = Self::sub_shares(&rhs, &b);
+
+                        let delta = self.reveal_shares(delta_share);
+                        let epsilon = self.reveal_shares(epsilon_share);
+
+                        async {
+                            let (delta, epsilon) = join!(delta, epsilon);
+                            c + b * epsilon.clone() + a * delta.clone() - epsilon.clone() * delta.clone()
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                join_all(multiplications).await
+            }
+        )
     }
 }
 
@@ -44,7 +68,7 @@ impl<P, T, S> MultiplicationScheme<T, S> for P
 
         Box::pin(
             async move {
-                let (a, b, c) = self.obtain_beaver_triples(1).await;
+                let (a, b, c) = self.obtain_beaver_triples(1).await.pop().unwrap();
 
                 let epsilon_share = Self::sub_shares(&lhs, &a);
                 let delta_share = Self::sub_shares(&rhs, &b);
