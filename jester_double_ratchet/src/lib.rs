f7e301431677bc8e629cf30cc2fde3abd0f01dd8
Cydhra <ubezl@student.kit.edu>
2019-12-30 11:26:07 +0100

    renamed members of ratchet with constant input

--- jester_double_ratchet/src/lib.rs
+++ jester_double_ratchet/src/lib.rs
@@ -12,13 +12,16 @@ trait KeyDerivationFunction<K, I, O> {
 }
 
 /// A symmetric key ratchet is a special case of an `KeyDerivationFunction` where the input is
-/// constant. It is therefore not a parameter to the `derive_key` function.
-trait SymmetricKeyRatchet<K, I, O>: KeyDerivationFunction<K, I, O> {
+/// constant. It is therefore not a parameter to the `derive_key_inputless` function.
+/// - `K` the derivation key type
+/// - `I` the data type of input to the derivation function
+/// - `O` the output key type
+trait ConstantInputKeyRatchet<K, I, O>: KeyDerivationFunction<K, I, O> {
     /// The constant input to the `KeyDerivationFunction`
     const INPUT: I;
 
     /// Consume the current `derivation_key` to generate a new chain key and an output key.
-    fn derive_key_symmetrically(derivation_key: K) -> (K, O) {
+    fn derive_key_inputless(derivation_key: K) -> (K, O) {
         Self::derive_key(derivation_key, Self::INPUT)
     }
 }
