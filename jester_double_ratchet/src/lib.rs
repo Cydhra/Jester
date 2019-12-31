e5a61ae3969d03fba9af14911104992ef5639748
Cydhra <ubezl@student.kit.edu>
2019-12-31 16:20:02 +0100

    use associated types for KDF parameters, instead of generics

--- jester_double_ratchet/src/lib.rs
+++ jester_double_ratchet/src/lib.rs
@@ -1,27 +1,27 @@
 /// A trait modelling a key-derivation-function as defined by the specification of the Double
 /// Ratchet Algorithm by Trevor Perrin and Moxie Marlinspike.
 ///
-/// #Type Parameters
-/// - `K` the derivation key type
-/// - `I` the data type of input to the derivation function
-/// - `O` the output key type
-trait KeyDerivationFunction<K, I, O> {
-    /// Consume the current `derivation_key` and `input` to generate a new derivation key and an
-    /// output key.
-    fn derive_key(derivation_key: K, input: I) -> (K, O);
+/// #Associated Types
+/// - `ChainKey` the derivation key type
+/// - `Input` the data type of input to the derivation function
+/// - `OutputKey` the output key type
+trait KeyDerivationFunction {
+    type ChainKey;
+    type Input;
+    type OutputKey;
+
+    /// Consume the current `chain_key` and `input` to generate a new derivation key and an output key.
+    fn derive_key(chain_key: Self::ChainKey, input: Self::Input) -> (Self::ChainKey, Self::OutputKey);
 }
 
 /// A symmetric key ratchet is a special case of an `KeyDerivationFunction` where the input is
-/// constant. It is therefore not a parameter to the `derive_key_inputless` function.
-/// - `K` the derivation key type
-/// - `I` the data type of input to the derivation function
-/// - `O` the output key type
-trait ConstantInputKeyRatchet<K, I, O>: KeyDerivationFunction<K, I, O> {
+/// constant. It is therefore not a parameter to the `derive_key_without_input` function.
+trait ConstantInputKeyRatchet: KeyDerivationFunction {
     /// The constant input to the `KeyDerivationFunction`
-    const INPUT: I;
+    const INPUT: Self::Input;
 
     /// Consume the current `derivation_key` to generate a new chain key and an output key.
-    fn derive_key_inputless(derivation_key: K) -> (K, O) {
+    fn derive_key_without_input(derivation_key: Self::ChainKey) -> (Self::ChainKey, Self::OutputKey) {
         Self::derive_key(derivation_key, Self::INPUT)
     }
 }
