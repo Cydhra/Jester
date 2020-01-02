5fcb003e148782d02a013252efa539d355232f47
Cydhra <ubezl@student.kit.edu>
2020-01-02 14:25:38 +0100

    proper documentation

--- jester_encryption/src/lib.rs
+++ jester_encryption/src/lib.rs
@@ -1,17 +1,20 @@
+//! A crate containing traits and protocols for creating and using encrypted channels. Specific implementations of
+//! encryption algorithms are not in scope of this crate, however a default implementation for the prime-field-based
+//! Diffie-Hellman-key-exchange protocol is provided.
+
 use rand::{CryptoRng, RngCore};
 
 pub mod diffie_hellman;
 
 /// A trait representing a symmetrical encryption scheme. It offers methods for generating a random key (though one
-/// might use a different scheme to generate a key) and encrypting and decrypting messages. No attempts for securing the
-/// keys are made, it is the caller's responsibility to properly protect the keys from attacker's attempts to
+/// might use a different scheme to generate a key) and encrypting and decrypting messages. No attempts are made to
+/// secure the key, it is the caller's responsibility to properly protect the keys from attacker's attempts to
 /// retrieve them from memory according to the caller's threat model.
 pub trait SymmetricalEncryptionScheme {
     type Key;
 
-    /// Generate a random key from the key space of this algorithm.
-    /// #Parameters
-    /// - `rng` a cryptographically secure random number generator
+    /// Randomly generate a random key from the key space of this algorithm. For this purpose, `rng` is assumed to be
+    /// an instance of a well-seeded, cryptographically secure random number generator.
     fn generate_key<R>(rng: &mut R) -> Self::Key
     where
         R: RngCore + CryptoRng;
@@ -24,9 +27,9 @@ pub trait SymmetricalEncryptionScheme {
 }
 
 /// A trait representing an asymmetrical encryption scheme. It offers methods for generating a random key pair and
-/// encrypting and decrypting messages. No attempts for securing the keys are made, it is the caller's responsibility
-/// to properly protect the keys from attacker's attempts to retrieve them from memory according to the caller's
-/// threat model.
+/// encrypting and decrypting messages. NNo attempts are made to secure the key, it is the caller's responsibility to
+/// properly protect the keys from attacker's attempts to retrieve them from memory according to the caller's threat
+/// model.
 pub trait AsymmetricalEncryptionScheme {
     type PrivateKey;
     type PublicKey;
