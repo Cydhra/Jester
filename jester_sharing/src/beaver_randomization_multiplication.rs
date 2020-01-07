6fd5ef2f93a254b90c25306af870df912d4ae53f
Cydhra <ubezl@student.kit.edu>
2020-01-07 16:20:41 +0100

    bugfix: beaver protocol subtracted where addition would have been correct

--- jester_sharing/src/beaver_randomization_multiplication.rs
+++ jester_sharing/src/beaver_randomization_multiplication.rs
@@ -5,15 +5,20 @@ use futures::{future::join_all, join};
 
 use jester_maths::prime::PrimeField;
 
-use crate::{CliqueCommunicationScheme, LinearSharingScheme, MultiplicationScheme, ParallelMultiplicationScheme, ThresholdSecretSharingScheme};
+use crate::{
+    CliqueCommunicationScheme, LinearSharingScheme, MultiplicationScheme,
+    ParallelMultiplicationScheme, ThresholdSecretSharingScheme,
+};
 
-type BoxFuture<'a, T> = Pin<Box<dyn Future<Output=T> + Send + 'a>>;
+type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 
 /// A trait marking a special instance of a parallel two-stage multiplication scheme using Donald Beaver's
 /// rerandomization technique.
 pub trait BeaverRandomizationMultiplication<T, S>: ParallelMultiplicationScheme<T, S>
-    where T: PrimeField,
-          Self: ThresholdSecretSharingScheme<T, S> {
+where
+    T: PrimeField,
+    Self: ThresholdSecretSharingScheme<T, S>,
+{
     /// Get the reconstruction threshold of the threshold secret sharing scheme used beneath this scheme.
     fn get_reconstruction_threshold(&self) -> usize;
 
@@ -23,80 +28,93 @@ pub trait BeaverRandomizationMultiplication<T, S>: ParallelMultiplicationScheme<
 }
 
 impl<P, T, S> ParallelMultiplicationScheme<T, S> for P
-    where T: PrimeField + Send + 'static,
-          S: Clone + Send + Sync + 'static,
-          P: BeaverRandomizationMultiplication<T, S> + ThresholdSecretSharingScheme<T, S> + LinearSharingScheme<T, S> +
-          CliqueCommunicationScheme<T, S> + MultiplicationScheme<T, S> + Send {
-    fn parallel_multiply<'a>(&'a mut self, pairs: &[(S, S)]) -> Pin<Box<dyn Future<Output=Vec<S>> + Send + 'a>> {
+where
+    T: PrimeField + Send + 'static,
+    S: Clone + Send + Sync + 'static,
+    P: BeaverRandomizationMultiplication<T, S>
+        + ThresholdSecretSharingScheme<T, S>
+        + LinearSharingScheme<T, S>
+        + CliqueCommunicationScheme<T, S>
+        + MultiplicationScheme<T, S>
+        + Send,
+{
+    fn parallel_multiply<'a>(
+        &'a mut self,
+        pairs: &[(S, S)],
+    ) -> Pin<Box<dyn Future<Output = Vec<S>> + Send + 'a>> {
         let pairs_clone: Vec<_> = pairs.to_vec();
 
-        Box::pin(
-            async move {
-                let beaver_triples = self.obtain_beaver_triples(pairs_clone.len()).await;
+        Box::pin(async move {
+            let beaver_triples = self.obtain_beaver_triples(pairs_clone.len()).await;
 
-                let multiplications = pairs_clone
-                    .into_iter()
-                    .zip(beaver_triples.clone())
-                    .map(|((lhs, rhs), (a, b, _))| {
-                        let epsilon_share = Self::sub_shares(&lhs, &a);
-                        let delta_share = Self::sub_shares(&rhs, &b);
+            let multiplications = pairs_clone
+                .into_iter()
+                .zip(beaver_triples.clone())
+                .map(|((lhs, rhs), (a, b, _))| {
+                    let epsilon_share = Self::sub_shares(&lhs, &a);
+                    let delta_share = Self::sub_shares(&rhs, &b);
 
-                        let delta = self.reveal_shares(delta_share);
-                        let epsilon = self.reveal_shares(epsilon_share);
+                    let delta = self.reveal_shares(delta_share);
+                    let epsilon = self.reveal_shares(epsilon_share);
 
-                        async {
-                            join!(delta, epsilon)
-                        }
-                    })
-                    .collect::<Vec<_>>();
+                    async { join!(delta, epsilon) }
+                })
+                .collect::<Vec<_>>();
 
-                join_all(multiplications).await
-                    .into_iter()
-                    .zip(beaver_triples)
-                    .map(|((delta, epsilon), (a, b, c))|
-                        Self::sub_scalar(
-                            &Self::add_shares(
-                                &Self::add_shares(&c, &Self::multiply_scalar(&b, &epsilon)),
-                                &Self::multiply_scalar(&a, &delta),
-                            ),
-                            &(epsilon.clone() * delta.clone()),
-                        )
+            join_all(multiplications)
+                .await
+                .into_iter()
+                .zip(beaver_triples)
+                .map(|((delta, epsilon), (a, b, c))| {
+                    Self::add_scalar(
+                        &Self::add_shares(
+                            &Self::add_shares(&c, &Self::multiply_scalar(&b, &epsilon)),
+                            &Self::multiply_scalar(&a, &delta),
+                        ),
+                        &(epsilon.clone() * delta.clone()),
                     )
-                    .collect()
-            }
-        )
+                })
+                .collect()
+        })
     }
 }
 
 impl<P, T, S> MultiplicationScheme<T, S> for P
-    where T: PrimeField + Send,
-          S: Clone + Send + Sync + 'static,
-          P: BeaverRandomizationMultiplication<T, S> + ThresholdSecretSharingScheme<T, S> + LinearSharingScheme<T, S> +
-          CliqueCommunicationScheme<T, S> + Send {
-    fn multiply<'a>(&'a mut self, lhs: &S, rhs: &S) -> Pin<Box<dyn Future<Output=S> + Send + 'a>> {
+where
+    T: PrimeField + Send,
+    S: Clone + Send + Sync + 'static,
+    P: BeaverRandomizationMultiplication<T, S>
+        + ThresholdSecretSharingScheme<T, S>
+        + LinearSharingScheme<T, S>
+        + CliqueCommunicationScheme<T, S>
+        + Send,
+{
+    fn multiply<'a>(
+        &'a mut self,
+        lhs: &S,
+        rhs: &S,
+    ) -> Pin<Box<dyn Future<Output = S> + Send + 'a>> {
         let lhs = lhs.clone();
         let rhs = rhs.clone();
 
-        Box::pin(
-            async move {
-                let (a, b, c) = self.obtain_beaver_triples(1).await.pop().unwrap();
+        Box::pin(async move {
+            let (a, b, c) = self.obtain_beaver_triples(1).await.pop().unwrap();
 
-                let epsilon_share = Self::sub_shares(&lhs, &a);
-                let delta_share = Self::sub_shares(&rhs, &b);
+            let epsilon_share = Self::sub_shares(&lhs, &a);
+            let delta_share = Self::sub_shares(&rhs, &b);
 
-                let (delta, epsilon) = join!(
-                    self.reveal_shares(delta_share),
-                    self.reveal_shares(epsilon_share)
-                );
+            let (delta, epsilon) = join!(
+                self.reveal_shares(delta_share),
+                self.reveal_shares(epsilon_share)
+            );
 
-                Self::sub_scalar(
-                    &Self::add_shares(
-                        &Self::add_shares(&c, &Self::multiply_scalar(&b, &epsilon)),
-                        &Self::multiply_scalar(&a, &delta),
-                    ),
-                    &(epsilon.clone() * delta.clone()),
-                )
-            }
-        )
+            Self::add_scalar(
+                &Self::add_shares(
+                    &Self::add_shares(&c, &Self::multiply_scalar(&b, &epsilon)),
+                    &Self::multiply_scalar(&a, &delta),
+                ),
+                &(epsilon.clone() * delta.clone()),
+            )
+        })
     }
 }
