95dc114c10dad5317782bd0e37e14f95f57420a7
Cydhra <ubezl@student.kit.edu>
2020-01-07 19:23:36 +0100

    moved implementations for tests into sub-mod

--- jester_sharing/src/protocols/mod.rs
+++ jester_sharing/src/protocols/mod.rs
@@ -15,85 +15,14 @@ mod random_number_generation;
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    use std::iter::repeat;
-    use std::pin::Pin;
+    use crate::CliqueCommunicationScheme;
+    use test_implementations::*;
 
     use futures::executor::block_on;
-    use futures::Future;
-    use mashup::*;
     use num::traits::{One, Zero};
     use rand::thread_rng;
 
-    use jester_maths::prime_fields;
-
-    use crate::beaver_randomization_multiplication::BeaverRandomizationMultiplication;
-    use crate::shamir_secret_sharing::ShamirSecretSharingScheme;
-    use crate::CliqueCommunicationScheme;
-
-    prime_fields!(TestPrimeField("7", 10));
-
-    /// A testing protocol that is carried out between two participants that do not randomize their inputs and do no
-    /// communicate as all values are deterministic anyways.
-    struct TestProtocol {
-        participant_id: usize,
-    }
-
-    impl ShamirSecretSharingScheme<TestPrimeField> for TestProtocol {}
-
-    /// All shares are considered to be carried out on polynomials where all coefficients are zero. Thus
-    /// communication is unnecessary and the secret is always the share
-    impl CliqueCommunicationScheme<TestPrimeField, (usize, TestPrimeField)> for TestProtocol
-    where
-        TestProtocol: ShamirSecretSharingScheme<TestPrimeField>,
-    {
-        fn reveal_shares(
-            &mut self,
-            share: (usize, TestPrimeField),
-        ) -> Pin<Box<dyn Future<Output = TestPrimeField> + Send>> {
-            Box::pin(async move { share.1 })
-        }
-
-        fn distribute_secret(
-            &mut self,
-            secret: TestPrimeField,
-        ) -> Pin<Box<dyn Future<Output = Vec<(usize, TestPrimeField)>> + Send>> {
-            let id = self.participant_id;
-            Box::pin(async move { vec![(id, secret.clone()), (id, secret)] })
-        }
-    }
-
-    impl BeaverRandomizationMultiplication<TestPrimeField, (usize, TestPrimeField)> for TestProtocol {
-        fn get_reconstruction_threshold(&self) -> usize {
-            2
-        }
-
-        fn obtain_beaver_triples<'a>(
-            &'a mut self,
-            count: usize,
-        ) -> Pin<
-            Box<
-                dyn Future<
-                        Output = Vec<(
-                            (usize, TestPrimeField),
-                            (usize, TestPrimeField),
-                            (usize, TestPrimeField),
-                        )>,
-                    > + Send
-                    + 'a,
-            >,
-        > {
-            Box::pin(async move {
-                repeat((
-                    (self.participant_id, TestPrimeField::one()),
-                    (self.participant_id, TestPrimeField::one()),
-                    (self.participant_id, TestPrimeField::one()),
-                ))
-                .take(count)
-                .collect()
-            })
-        }
-    }
+    mod test_implementations;
 
     #[test]
     fn test_protocol() {
