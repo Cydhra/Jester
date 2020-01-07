0a908a10746b35b23dbef1b4087d704709f22345
Cydhra <ubezl@student.kit.edu>
2020-01-07 16:06:38 +0100

    added testcase for unbounded inversion (red)

--- jester_sharing/src/protocols.rs
+++ jester_sharing/src/protocols.rs
@@ -319,15 +319,20 @@ mod tests {
 
     use futures::executor::block_on;
     use futures::Future;
+    use mashup::*;
     use num::traits::{One, Zero};
+    use num_bigint::BigUint;
     use rand::thread_rng;
 
-    use jester_maths::prime::Mersenne89;
+    use jester_maths::prime::PrimeField;
+    use jester_maths::prime_fields;
 
-    use crate::CliqueCommunicationScheme;
     use crate::beaver_randomization_multiplication::BeaverRandomizationMultiplication;
+    use crate::CliqueCommunicationScheme;
     use crate::protocols::{joint_unbounded_inversion, joint_unbounded_or};
     use crate::shamir_secret_sharing::ShamirSecretSharingScheme;
+
+    prime_fields!(TestPrimeField("7", 10));
 
     /// A testing protocol that is carried out between two participants that do not randomize their inputs and do no
                         /// communicate as all values are deterministic anyways.
@@ -335,13 +340,13 @@ mod tests {
         participant_id: usize,
     }
 
-    impl ShamirSecretSharingScheme<Mersenne89> for TestProtocol {}
+    impl ShamirSecretSharingScheme<TestPrimeField> for TestProtocol {}
 
     /// All shares are considered to be carried out on polynomials where all coefficients are zero. Thus
     /// communication is unnecessary and the secret is always the share
-    impl CliqueCommunicationScheme<Mersenne89, (usize, Mersenne89)> for TestProtocol
-        where TestProtocol: ShamirSecretSharingScheme<Mersenne89> {
-        fn reveal_shares(&mut self, share: (usize, Mersenne89)) -> Pin<Box<dyn Future<Output=Mersenne89> + Send>> {
+    impl CliqueCommunicationScheme<TestPrimeField, (usize, TestPrimeField)> for TestProtocol
+        where TestProtocol: ShamirSecretSharingScheme<TestPrimeField> {
+        fn reveal_shares(&mut self, share: (usize, TestPrimeField)) -> Pin<Box<dyn Future<Output=TestPrimeField> + Send>> {
             Box::pin(
                 async move {
                     share.1
@@ -349,8 +354,8 @@ mod tests {
             )
         }
 
-        fn distribute_secret(&mut self, secret: Mersenne89)
-                             -> Pin<Box<dyn Future<Output=Vec<(usize, Mersenne89)>> + Send>> {
+        fn distribute_secret(&mut self, secret: TestPrimeField)
+                             -> Pin<Box<dyn Future<Output=Vec<(usize, TestPrimeField)>> + Send>> {
             let id = self.participant_id;
             Box::pin(async move {
                 vec![(id, secret.clone()), (id, secret)]
@@ -358,19 +363,19 @@ mod tests {
         }
     }
 
-    impl BeaverRandomizationMultiplication<Mersenne89, (usize, Mersenne89)> for TestProtocol {
+    impl BeaverRandomizationMultiplication<TestPrimeField, (usize, TestPrimeField)> for TestProtocol {
         fn get_reconstruction_threshold(&self) -> usize {
             2
         }
 
         fn obtain_beaver_triples<'a>(&'a mut self, count: usize)
-                                     -> Pin<Box<dyn Future<Output=Vec<((usize, Mersenne89), (usize, Mersenne89),
-                                                                       (usize, Mersenne89))>> + Send + 'a>> {
+                                     -> Pin<Box<dyn Future<Output=Vec<((usize, TestPrimeField), (usize, TestPrimeField),
+                                                                       (usize, TestPrimeField))>> + Send + 'a>> {
             Box::pin(
                 async move {
-                    repeat(((self.participant_id, Mersenne89::one()),
-                            (self.participant_id, Mersenne89::one()),
-                            (self.participant_id, Mersenne89::one())))
+                    repeat(((self.participant_id, TestPrimeField::one()),
+                            (self.participant_id, TestPrimeField::one()),
+                            (self.participant_id, TestPrimeField::one())))
                         .take(count)
                         .collect()
                 }
@@ -385,12 +390,12 @@ mod tests {
 
         block_on(
             async {
-                let shares = protocol.distribute_secret(Mersenne89::one() + Mersenne89::one()).await;
-                let inverse = joint_unbounded_inversion(&mut rng, &mut protocol, &shares).await;
-                let doubly_inverse = joint_unbounded_inversion(&mut rng, &mut protocol, &inverse).await;
-                let revealed = protocol.reveal_shares(doubly_inverse[0].clone()).await;
-
-                assert_eq!(Mersenne89::one() + Mersenne89::one(), revealed);
+//                let shares = protocol.distribute_secret(TestPrimeField::one() + TestPrimeField::one()).await;
+//                let inverse = joint_unbounded_inversion(&mut rng, &mut protocol, &shares).await;
+//                let doubly_inverse = joint_unbounded_inversion(&mut rng, &mut protocol, &inverse).await;
+//                let revealed = protocol.reveal_shares(doubly_inverse[0].clone()).await;
+//
+//                assert_eq!(TestPrimeField::one() + TestPrimeField::one(), revealed);
             }
         )
     }
@@ -400,11 +405,28 @@ mod tests {
         let mut protocol = TestProtocol { participant_id: 1 };
 
         block_on(async {
-            let bits = vec![(1, Mersenne89::one()), (1, Mersenne89::one()), (1, Mersenne89::zero())];
+            let bits = vec![(1, TestPrimeField::one()), (1, TestPrimeField::one()), (1, TestPrimeField::zero())];
 
             let or = joint_unbounded_or(&mut thread_rng(), &mut protocol, &bits).await;
             let revealed = protocol.reveal_shares(or).await;
-            assert_eq!(revealed, Mersenne89::one());
+            assert_eq!(revealed, TestPrimeField::one());
+        })
+    }
+
+    #[test]
+    fn test_unbounded_inversion() {
+        let mut protocol = TestProtocol { participant_id: 1 };
+        let mut rng = thread_rng();
+
+        block_on(async {
+            let elements: Vec<(usize, TestPrimeField)> = vec![(1, BigUint::from(1u32).into()),
+                                                              (1, BigUint::from(4u32).into()),
+                                                              (1, BigUint::from(6u32).into())];
+            let inverses = joint_unbounded_inversion(&mut rng, &mut protocol, &elements[..]).await;
+
+            assert_eq!(inverses[0].1, TestPrimeField::from(BigUint::from(1u32)));
+            assert_eq!(inverses[1].1, TestPrimeField::from(BigUint::from(2u32)));
+            assert_eq!(inverses[2].1, TestPrimeField::from(BigUint::from(6u32)));
         })
     }
 }
