4daeffe4164d649162611b86068e7b1fd6781844
Cydhra <ubezl@student.kit.edu>
2021-01-13 16:47:36 +0100

    finished blake2b implementation

--- jester_hashes/src/blake/blake2b.rs
+++ jester_hashes/src/blake/blake2b.rs
@@ -1,5 +1,9 @@
+use std::convert::TryInto;
+use std::mem;
+use std::ptr::hash;
+
+use crate::{HashFunction, HashValue};
 use crate::blake::{blake2_mix, SIGMA};
-use crate::{HashValue, HashFunction};
 
 /// The initial state for any blake2b hash. From here, all blocks are applied.
 pub const INITIAL_2B: Blake2bHash = Blake2bHash([
@@ -21,41 +25,150 @@ pub const BLAKE_2B_BLOCK_SIZE: usize = 128;
 #[derive(Debug, Copy, Clone)]
 pub struct Blake2bHash([u64; 8]);
 
-pub struct Blake2bContext {}
+pub struct Blake2bContext {
+    output_len: usize,
+    key: Vec<u8>,
+}
 
 pub struct Blake2bState {
     hash: Blake2bHash,
     message_length: u128,
+    remaining_data_buffer: [u8; BLAKE_2B_BLOCK_SIZE],
+    remaining_data_length: usize,
 }
 
 impl HashFunction for Blake2bHash {
     type Context = Blake2bContext;
     type HashState = Blake2bState;
-    type HashData = Blake2bState;
+    type HashData = Blake2bHash;
 
     fn init_hash(ctx: &Self::Context) -> Self::HashState {
         // TODO keyed hashing using context
-        Blake2bState { hash: INITIAL_2B, message_length: 0 }
+        let mut state = Blake2bState {
+            hash: INITIAL_2B,
+            message_length: 0,
+            remaining_data_buffer: [0_u8; BLAKE_2B_BLOCK_SIZE],
+            remaining_data_length: 0,
+        };
+
+        // parameter block
+        state.hash.0[0] ^= 0x0101_0000 ^ ((ctx.key.len() as u64) << 8) ^ ctx.output_len as u64;
+
+        // copy the key into the remaining data buffer and set the buffer to full. However, do
+        // not compress yet: If no further data is hashed, this is considered the last block,
+        // thus we cannot know whether the last block flag must be set.
+        if !ctx.key.is_empty() {
+            state.remaining_data_buffer[..ctx.key.len()].copy_from_slice(&ctx.key);
+            state.remaining_data_length = BLAKE_2B_BLOCK_SIZE;
+        }
+
+        state
     }
 
     fn update_hash(hash: &mut Self::HashState, ctx: &Self::Context, input: &[u8]) {
-        unimplemented!()
+        // offset where to begin reading input data
+        let mut input_data_offset = 0;
+
+        // check whether at least one block can be compressed
+        if hash.remaining_data_length + input.len() > BLAKE_2B_BLOCK_SIZE {
+            // if there is remaining data in the buffer, compress it with the new input appended
+            if hash.remaining_data_length > 0 {
+                input_data_offset = BLAKE_2B_BLOCK_SIZE - hash.remaining_data_length;
+                let mut block = [0_u8; 128];
+                block[..hash.remaining_data_length]
+                    .copy_from_slice(&hash.remaining_data_buffer[..hash.remaining_data_length]);
+                block[hash.remaining_data_length..]
+                    .copy_from_slice(&input[..input_data_offset]);
+
+                blake2b_compress(hash, &block, false);
+
+                hash.remaining_data_length = 0;
+            }
+        } else { // else just add the input to the buffer and return from the function
+            hash.remaining_data_buffer[hash.remaining_data_length..
+                hash.remaining_data_length + input.len()]
+                .copy_from_slice(&input[..]);
+            hash.remaining_data_length += input.len();
+        }
+
+        // compress full blocks from the input buffer except the last one
+        let block_count = (input.len() - input_data_offset) / BLAKE_2B_BLOCK_SIZE;
+        for i in 0..block_count - 1 {
+            blake2b_compress(
+                hash,
+                &input[input_data_offset + i * BLAKE_2B_BLOCK_SIZE..
+                    input_data_offset + (i + 1) * BLAKE_2B_BLOCK_SIZE].try_into().unwrap(),
+                false,
+            )
+        }
+
+        // if there is more data in the input buffer, the last full block is not the last block
+        // of the algorithm and can therefore be compressed safely
+        if input_data_offset + block_count * BLAKE_2B_BLOCK_SIZE < input.len() {
+            blake2b_compress(
+                hash,
+                &input[input_data_offset + (block_count - 1) * BLAKE_2B_BLOCK_SIZE..
+                    input_data_offset + block_count * BLAKE_2B_BLOCK_SIZE].try_into().unwrap(),
+                false,
+            );
+
+            hash.remaining_data_length = input.len() -
+                (input_data_offset + block_count * BLAKE_2B_BLOCK_SIZE);
+            hash.remaining_data_buffer[..hash.remaining_data_length]
+                .copy_from_slice(&input[input_data_offset + block_count * BLAKE_2B_BLOCK_SIZE..]);
+        }
+        // otherwise this could potentially be the last block, therefore only add it to the buffer
+        else {
+            hash.remaining_data_length = BLAKE_2B_BLOCK_SIZE;
+            hash.remaining_data_buffer[..].copy_from_slice(&input[input_data_offset
+                + (block_count - 1) * BLAKE_2B_BLOCK_SIZE..]);
+        }
     }
 
     fn finish_hash(hash: &mut Self::HashState, ctx: &Self::Context) -> Self::HashData {
-        unimplemented!()
+        if hash.message_length.wrapping_add(hash.remaining_data_length as u128) <
+            hash.message_length {
+            panic!("blake2b cannot hash more than 2**128-1 bytes")
+        } else {
+            hash.message_length += hash.remaining_data_length as u128;
+        }
+
+        // pad last block with zeros
+        let mut last_block = [0_u8; BLAKE_2B_BLOCK_SIZE];
+        last_block[..hash.remaining_data_length]
+            .copy_from_slice(&hash.remaining_data_buffer[..hash.remaining_data_length]);
+
+        blake2b_compress(hash, &last_block, true);
+
+        // TODO change output length according to context
+        hash.hash
     }
 
     fn digest_message(ctx: &Self::Context, input: &[u8]) -> Self::HashData {
         let mut hash_state = Self::init_hash(ctx);
+
         Self::update_hash(&mut hash_state, ctx, input);
         Self::finish_hash(&mut hash_state, ctx)
     }
 }
 
-impl HashValue for Blake2bState {
+impl HashValue for Blake2bHash {
     fn raw(&self) -> Box<[u8]> {
-        unimplemented!()
+        unsafe {
+            // TODO: do this properly
+            mem::transmute::<[u64; 8], [u8; 64]>([
+                u64::from_le(self.0[0]),
+                u64::from_le(self.0[1]),
+                u64::from_le(self.0[2]),
+                u64::from_le(self.0[3]),
+                u64::from_le(self.0[4]),
+                u64::from_le(self.0[5]),
+                u64::from_le(self.0[6]),
+                u64::from_le(self.0[7]),
+            ])
+        }
+            .to_vec()
+            .into()
     }
 }
 
@@ -64,19 +177,27 @@ fn blake2b_mix(vector: &mut [u64; 16], a: usize, b: usize, c: usize, d: usize, x
     blake2_mix::<u64, 32, 24, 16, 63>(vector, a, b, c, d, x, y)
 }
 
-fn blake2b_compress(state: &mut [u64; 8], iv: &[u64; 8], input_block: &[u64; 16], byte_count: u128,
-                    last_block: bool) {
+fn blake2b_compress(state: &mut Blake2bState, input: &[u8; 128], last_block: bool) {
     // initialize local working vector
     let mut vector: [u64; 16] = [0; 16];
-    vector[0..=7].copy_from_slice(&state[..]);
-    vector[8..=15].copy_from_slice(&iv[..]);
+    vector[0..=7].copy_from_slice(&state.hash.0[..]);
+    vector[8..=15].copy_from_slice(&INITIAL_2B.0[..]);
 
-    vector[12] ^= byte_count as u64;
-    vector[13] ^= (byte_count >> BLAKE_2B_WORD_LENGTH) as u64;
+    // add message length
+    if state.message_length.wrapping_add(BLAKE_2B_BLOCK_SIZE as u128) > state.message_length {
+        state.message_length += BLAKE_2B_BLOCK_SIZE as u128;
+    } else {
+        panic!("blake2b cannot hash more than 2**128-1 bytes")
+    }
+
+    vector[12] ^= state.message_length as u64;
+    vector[13] ^= (state.message_length >> 64) as u64;
 
     if last_block {
         vector[14] ^= u64::MAX
     }
+
+    let input_block = transform_block(input);
 
     for i in 0..BLAKE_2B_ROUND_COUNT {
         let permutation = &SIGMA[i % 10][0..16];
@@ -109,7 +230,14 @@ fn blake2b_compress(state: &mut [u64; 8], iv: &[u64; 8], input_block: &[u64; 16]
     }
 
     for i in 0..8 {
-        state[i] ^= vector[i] ^ vector[i + 8];
+        state.hash.0[i] ^= vector[i] ^ vector[i + 8];
     }
 }
 
+fn transform_block(bytes: &[u8; 128]) -> [u64; 16] {
+    let mut block = [0_u64; 16];
+    for i in 0..16 {
+        block[i] = u64::from_le_bytes(bytes[i * 8..(i + 1) * 8].try_into().unwrap());
+    }
+    block
+}
